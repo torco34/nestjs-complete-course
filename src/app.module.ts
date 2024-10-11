2197277d94b5138bb806131f6ddde476f10fb72c
torco34 <torcoromaarias@gmail.com>
2024-10-11 08:16:52 -0500

    Carpetas creadas

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
+})
+export class AppModule {}
